27e66a88f978a0df1af03af1b4e2014fe7698194
claudeilton dantas <fcd007@hotmail.com>
2020-12-08 20:55:07 -0300

    update core game messages

---
+++ ForceMemory/aula07-mensagens-utilidades/src/jogoDaMemoria.js
@@ -0,0 +1,111 @@
+class JogoDaMemoria {
+    //se mandar um obj = {tela: 1, idade: 20, outros: 2}
+    //ele ira ignorar o resto das propriedades e pegar somente a propriedade tela
+    constructor({ tela }) {
+        this.tela = tela
+        //o caminho do arquivo relativo ao index.html
+        this.heroisIniciais = [
+            { img: './img/batman.png', nome:'batman'},
+            { img: './img/capitao.png', nome:'capitao'},
+            { img: './img/mulher.png', nome:'mulher'},
+            { img: './img/superman.png', nome:'superman'}
+        ]
+
+        this.iconePadrao = './img/heroi.png'
+        this.heroisEscondidos = []
+        this.heroisSelecionados = []
+    }
+    // para o uso do this, não podemos usar o static
+    inicializar() {
+        //vamos pegar todas as funções da classe tela
+        //coloca todos os herois na tela
+        this.tela.atualizarImagens(this.heroisIniciais)
+        // forçaa a tela a usar o THIS de Jogo da memoria
+        this.tela.configurarBotaoJogar(this.jogar.bind(this))
+        // 
+        this.tela.configurarBotaoVerificarSelecao(this.verificarSelecao.bind(this))
+    }
+
+     // lógica para embaralhar o jogo da memória
+     embaralhar() {
+        const copias = this.heroisIniciais
+        //duplicar os itens
+        .concat(this.heroisIniciais)
+        //vamos gerar os ids e depois usar
+        .map( item => {
+            return Object.assign({}, item, { id: Math.random() / 0.5})
+        })
+        //ordenar de forma aleatoriamente
+        .sort(() => Math.random() - 0.5)
+        //vamos verificar se as imagens estão atualizando
+        this.tela.atualizarImagens(copias)
+        //vamos fazer a tela esperar 1 segundo para atualizar
+        setTimeout(() => {
+            this.esconderHerois(copias)
+        }, 1000);
+    }
+    //ele vai trocar a imagem de todos os herois pela icone padrão
+    //como fizemos no construtor, vamos  extrai somente o que
+    //precisamos, usando sintaxe ({ chave: 1 }) retornar o que tiver 
+    //dentro dos parenteses
+    esconderHerois(herois) {
+        const heroisOcultos = herois.map(({ nome, id }) => ({
+            id, //o js olhar o contexto atual e vai pegar o valor id
+            nome, // do mesmo modo irá fazer com valor padrão já no contexto
+            img: this.iconePadrao //aqui usamos a imagem padrão substituindo
+        }))
+        //atualizando a tela com os herois ocultos
+        this.tela.atualizarImagens(heroisOcultos)
+        //guardar os herois para trabalhar com eles depois
+        this.heroisOcultos = heroisOcultos;
+    }
+
+    exibirHerois(nomeDoHerois) {
+        //vamos procurar o heroi pelo nome em nossos heroisInicias
+        //vamos obter somente a imagem dele
+        const { img } = this.heroisIniciais.find(({ nome }) => nomeDoHerois === nome)
+        //vamos criar a função na tela, para exibir somente heroi selecionado
+        // console.log(nomeDoHerois)
+        this.tela.exibirHerois(nomeDoHerois, img)
+        // console.log(nomeDoHerois, img)
+    }
+
+    verificarSelecao(id, nome) {
+        const item = { id, nome }
+        //bora verificar a quantidade de herois selecionados
+        // e depois tomar ação se escolheu certo ou errado
+        const heroisSelecionados = this.heroisSelecionados.length
+        switch (heroisSelecionados) {
+            case 0:
+                //adicionar a escolha a uma lista, esperando pela próxima
+                //vez que clicar
+                this.heroisSelecionados.push(item)
+                break;
+            case 1:
+                // se a quantidade for 1 o usuário só pode escolher mais um
+                //vamos obter o primeiro item da lista
+                const [opcao1] = this.heroisSelecionados
+                //zerar a lista anterior
+                this.heroisSelecionados = []
+                //conferimos se os nomes e ids batem conforme o esperado
+                if (opcao1.nome === item.nome && opcao1.id !== item.id) {
+                    //aqui verificamos se são ids diferentes para o 
+                    //usuário não clicar duas vezes no mesmo heroi
+                    this.exibirHerois(item.nome)
+                    //como padrão e true, não precisa passar nada
+                    this.tela.exibirMensagem()
+                    // console.log(item.nome)
+                    return;
+                }
+                //caso tenha erro exibe mensagem (false)
+                this.tela.exibirMensagem(false)
+                // alert('Combinação incorreta!')
+                //fim do case
+            break;
+        }
+    }
+
+    jogar() {
+        this.embaralhar()
+    }
+}
---
+++ ForceMemory/aula07-mensagens-utilidades/src/tela.js
@@ -0,0 +1,79 @@
+const ID_CONTEUDO = "conteudo"
+const ID_BTN_JOGAR = "jogar"
+//mensagem de alerta acerto ou erro
+const ID_MENSAGEM = "mensagem"
+const CLASSE_INVISIBLE = "invisible"
+
+const MENSAGENS = {
+    sucesso: {
+        texto: 'Combinação correta!',
+        classe: 'alert-success'
+    },
+    erro: {
+        texto: 'Combinação incorreta!',
+        classe: 'alert-danger'
+    }
+}
+
+class Tela {
+    static obterCodigoHTML(item) {
+        return`
+        <div class="col-md-3">
+            <div class="card" style="width: 12rem;" onclick="window.verificarSelecao('${item.id}','${item.nome}')">
+                <img src="${item.img}" name="${item.nome}" class="card-img-top" alt="...">
+            </div>
+            <br />
+        </div>
+        `
+    }
+
+    static configurarBotaoVerificarSelecao(funcaoOnClick) {
+        window.verificarSelecao = funcaoOnClick
+    }
+
+    static alterConteudodoHTML(codigoHtml) {
+        const conteudo = document.getElementById(ID_CONTEUDO)
+        //usando para passar código gerado HTML
+        conteudo.innerHTML = codigoHtml;
+    }
+
+    static gerarStringPelaImagem(itens) {
+        //para cada item da lista, vai executar a função obter código
+        //ao final, vai concatenar tudo em uma única string
+        //muda de Array para String/
+        return itens.map(Tela.obterCodigoHTML).join('')
+    }
+    //vamos pegar os itens iamgens e alterar dinamicamente
+    static atualizarImagens(itens) {
+        const codigoHtml = this.gerarStringPelaImagem(itens);
+        Tela.alterConteudodoHTML(codigoHtml);
+    }
+
+    //vamos pegar o click e acionar funcionalidade jogar
+    static configurarBotaoJogar(funcaoOnClick) {
+        const btnJogar = document.getElementById(ID_BTN_JOGAR)
+        btnJogar.onclick = funcaoOnClick
+    }
+
+    static exibirHerois(nomeDoHeroi, img) {
+        const elementosHtml = document.getElementsByName(nomeDoHeroi)
+        //para cada elemento encontrado na tela, vamos alterar a imagem
+        //para a imagem inicial dele
+        //com o forEach, para cada item, dentro dos () setamos o valor de imagem
+        elementosHtml.forEach(item => (item.src = img))
+    }
+    //por padrão iremos utilizar o sucesso = true
+    static exibirMensagem(sucesso = true) {
+        const elemento = document.getElementById(ID_MENSAGEM)
+        if(sucesso){
+            elemento.classList.remove(MENSAGENS.erro.classe)
+            elemento.classList.add(MENSAGENS.sucesso.classe)
+            elemento.innerText = MENSAGENS.sucesso.texto
+        }else {
+            elemento.classList.remove(MENSAGENS.sucesso.classe)
+            elemento.classList.add(MENSAGENS.erro.classe)
+            elemento.innerText = MENSAGENS.erro.texto
+        }
+        elemento.classList.remove(CLASSE_INVISIBLE)
+    }
+}
